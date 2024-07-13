d4e9cf811e5466493b585508b3959b28a16d84d4
JinMinu <s9430939@naver.com>
2024-07-14 02:09:50 +0900

    complete

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+const API_KEY = "e5b6c23dab5b08d50c870d20c37863a7";
+
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    console.log("You live in", lat, lon);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url).then(Response => Response.json().then(data => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        city.innerText = data.name;
+    }));
+}
+
+function onGeoErr() {
+    alert("Can't find you")
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoErr);
